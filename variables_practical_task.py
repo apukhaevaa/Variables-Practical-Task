8572a1b352e24244d899f1b7fdae4b7471975df6
apukhaevaa <apukhaevaa@gmail.com>
2024-08-24 20:04:10 +0300

    Update variables_practical_task.py

--- variables_practical_task.py
+++ variables_practical_task.py
@@ -6,4 +6,5 @@ course_name = 'Python'
 time_per_task = hours_spent / tasks_done
 print(tasks_done, hours_spent, course_name, time_per_task)
 print('Курс: ' + course_name + ',', 'всего задач:', tasks_done, ',', 'затрачено часов:', hours_spent, ',', 'среднее время выполнения', time_per_task, 'часа.')
-
+# то что мы не проходили, запись через функцию
+print(f'Курс: {course_name}, всего задач:{tasks_done}, затрачено часов: {hours_spent}, среднее время выполнения {time_per_task} часа.')
