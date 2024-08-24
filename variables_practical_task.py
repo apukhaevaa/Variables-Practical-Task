6de769d2bfffbfb982476b6c8ffbdb5b6875d33a
apukhaevaa <apukhaevaa@gmail.com>
2024-08-24 19:43:56 +0300

    Add files via upload

---
+++ variables_practical_task.py
@@ -0,0 +1,9 @@
+# Пухаева Алина Александровна
+# Variables: Practical Task
+tasks_done = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / tasks_done
+print(tasks_done, hours_spent, course_name, time_per_task)
+print('Курс: ' + course_name + ',', 'всего задач:', tasks_done, ',', 'затрачено часов:', hours_spent, ',', 'среднее время выполнения', time_per_task, 'часа.')
+
